921abd784dcef7d257deaeb2f6292a2b5a15e94e
Suhana <suhanaaa@google.com>
2026-04-07 06:01:03 +0530

    refactoring:

--- keras/src/backend/torch/core.py
+++ keras/src/backend/torch/core.py
@@ -233,50 +233,50 @@ def convert_to_tensor(x, dtype=None, sparse=None, ragged=None):
                 x = x.to(device)
         if dtype is not None:
             x = x.to(to_torch_dtype(dtype))
-    else:
-        if dtype is None:
-            if isinstance(x, bool):
-                x = torch.as_tensor(x, dtype=torch.bool, device=get_device())
-            elif isinstance(x, int):
-                if x < -(2**31) or x >= 2**31:
-                    x = torch.as_tensor(
-                        x, dtype=torch.int64, device=get_device()
-                    )
-                else:
-                    x = torch.as_tensor(
-                        x, dtype=torch.int32, device=get_device()
-                    )
-            elif isinstance(x, float):
+
+    if dtype is None and not is_tensor(x):
+        if isinstance(x, bool):
+            x = torch.as_tensor(x, dtype=torch.bool, device=get_device())
+        elif isinstance(x, int):
+            if x < -(2**31) or x >= 2**31:
                 x = torch.as_tensor(
-                    x, dtype=to_torch_dtype(floatx()), device=get_device()
+                    x, dtype=torch.int64, device=get_device()
                 )
+            else:
+                x = torch.as_tensor(
+                    x, dtype=torch.int32, device=get_device()
+                )
+        elif isinstance(x, float):
+            x = torch.as_tensor(
+                x, dtype=to_torch_dtype(floatx()), device=get_device()
+            )
+
+    if not is_tensor(x):
+        # Convert to np in case of any array-like that is not list or tuple.
+        if not isinstance(x, (list, tuple)):
+            x = np.array(x)
+        elif len(x) > 0 and any(isinstance(x1, torch.Tensor) for x1 in x):
+            # Handle list or tuple of torch tensors
+            x = torch.stack([convert_to_tensor(x1) for x1 in x])
 
         if not is_tensor(x):
-            # Convert to np in case of any array-like that is not list or tuple.
-            if not isinstance(x, (list, tuple)):
-                x = np.array(x)
-            elif len(x) > 0 and any(isinstance(x1, torch.Tensor) for x1 in x):
-                # Handle list or tuple of torch tensors
-                x = torch.stack([convert_to_tensor(x1) for x1 in x])
-
-            if not is_tensor(x):
-                if isinstance(x, np.ndarray):
-                    if x.dtype == np.uint32:
-                        # Torch backend does not support uint32.
-                        x = x.astype(np.int64)
-                    if standardize_dtype(x.dtype) == "bfloat16":
-                        x = x.astype(np.float32)
-                        dtype = "bfloat16"
-                    dtype = dtype or x.dtype
-                if dtype is None:
-                    dtype = result_type(
-                        *[
-                            getattr(item, "dtype", type(item))
-                            for item in tree.flatten(x)
-                        ]
-                    )
-                dtype = to_torch_dtype(dtype)
-                x = torch.as_tensor(x, dtype=dtype, device=get_device())
+            if isinstance(x, np.ndarray):
+                if x.dtype == np.uint32:
+                    # Torch backend does not support uint32.
+                    x = x.astype(np.int64)
+                if standardize_dtype(x.dtype) == "bfloat16":
+                    x = x.astype(np.float32)
+                    dtype = "bfloat16"
+                dtype = dtype or x.dtype
+            if dtype is None:
+                dtype = result_type(
+                    *[
+                        getattr(item, "dtype", type(item))
+                        for item in tree.flatten(x)
+                    ]
+                )
+            dtype = to_torch_dtype(dtype)
+            x = torch.as_tensor(x, dtype=dtype, device=get_device())
 
     from keras.src.distribution import distribution_lib as dist_lib
 
@@ -292,7 +292,7 @@ def convert_to_tensor(x, dtype=None, sparse=None, ragged=None):
             from keras.src.distribution import TensorLayout
 
             layout = TensorLayout([None] * x.ndim, dist.device_mesh)
-            x = torch_dist_lib.distribute_tensor(x, layout)
+            x = dist_lib.distribute_tensor(x, layout)
     return x
 
 
